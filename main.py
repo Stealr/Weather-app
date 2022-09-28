a2270de2ba4d59cfe61579f24e584ccddd4d15c8
Stealr <baranovm003@yandex.ru>
2022-09-28 23:40:35 +0300

    Прогноз на неделю

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import requests
 
 city = "Moscow,RU"
 appid = "57aa997149b0db17f5e2a70a573bb50f"
-res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
                    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
 
-print("Город", city)
-print("Погодные условия:", data['weather'][0]['description'])
-print("Температура:", data['main']['temp'])
-print("Минимальная температура:", data['main']['temp_min'])
-print("Максимальная температура:", data['main']['temp_max'])
+print(data)
+
+print('Прогноз погоды на неделю:')
+for i in data['list']:
+    print("Дата<", i['dt_txt'], ">\r\nТемпература<", '{0:+3.0f}'.format(i['main']['temp']), "\r\nПогодные условия<",
+          i['weather'][0]['description'], ">")
+    print("------------------------------")
+# print("Город", city)
+# print("Погодные условия:", data['weather'][0]['description'])
+# print("Температура:", data['main']['temp'])
+# print("Минимальная температура:", data['main']['temp_min'])
+# print("Максимальная температура:", data['main']['temp_max'])
